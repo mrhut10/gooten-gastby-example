91bca96b8d1db8d47cfe0faf91c324160432e548
mrhut10 <mrhut10@protonmail.com>
2020-03-05 13:37:32 +1100

    lint index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { graphql, useStaticQuery } from 'gatsby'
-import QueryStringToCode from '../components/QueryStringToCode'
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { graphql, useStaticQuery } from "gatsby";
+import QueryStringToCode from "../components/QueryStringToCode";
 import ProductNodeToTile from "../components/ProductNodeToTile";
-// import ProductNodeToTile from '../components/ProductNodeToTile'
 
 const productQueryString = `
 {
@@ -18,41 +17,46 @@ const productQueryString = `
       ShortDescription
     }
   }
-}`
+}`;
 const productQuery = graphql`
-{
-  allGootenProduct(limit: 12) {
-    nodes {
-      Name
-      Images {
-        ImageTypes
-        Url
+  {
+    allGootenProduct(limit: 12) {
+      nodes {
+        Name
+        Images {
+          ImageTypes
+          Url
+        }
+        ShortDescription
       }
-      ShortDescription
     }
   }
-}
-`
+`;
 
 function IndexPage() {
-  const { allGootenProduct : { nodes: ProductNodes } } = useStaticQuery(productQuery);
+  const {
+    allGootenProduct: { nodes: ProductNodes }
+  } = useStaticQuery(productQuery);
 
   return (
     <Layout>
       <SEO
-        keywords={[`gatsby`, `gooten`, 'ecommerce', 'dropshipping']}
+        keywords={[`gatsby`, `gooten`, "ecommerce", "dropshipping"]}
         title="Home"
       />
 
       <section className="text-center">
-        
         <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
           Integrate Gooten Api into Your Gatsby JS Page
         </h2>
         <p className="leading-loose">
-          Below is an example of first 9 products returned from API
+          Example of first 12 products returned from the Gooten API
         </p>
-        <QueryStringToCode className="m-md p-md" title="GraphQl Query" queryString={productQueryString}/>
+        <QueryStringToCode
+          className="m-md p-md"
+          title="GraphQl Query"
+          queryString={productQueryString}
+        />
         <div className="flex flex-wrap shadow-lg container w-full p-md m-md">
           {ProductNodes.map(ProductNodeToTile)}
         </div>
