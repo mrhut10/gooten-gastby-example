ebbaa3fb8fd37aab4958dd76b77bc606b51959de
mrhut10 <mrhut10@protonmail.com>
2020-03-05 12:48:56 +1100

    played with index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,42 @@
 import React from "react";
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { graphql, useStaticQuery } from 'gatsby'
+import QueryStringToCode from '../components/QueryStringToCode'
+import ProductNodeToTile from "../components/ProductNodeToTile";
+// import ProductNodeToTile from '../components/ProductNodeToTile'
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+const productQueryString = `
+{
+  allGootenProduct(limit: 12) {
+    nodes {
+      Name
+      Images {
+        ImageTypes
+        Url
+      }
+      ShortDescription
+    }
+  }
+}`
+const productQuery = graphql`
+{
+  allGootenProduct(limit: 12) {
+    nodes {
+      Name
+      Images {
+        ImageTypes
+        Url
+      }
+      ShortDescription
+    }
+  }
+}
+`
 
 function IndexPage() {
+  const { allGootenProduct : { nodes: ProductNodes } } = useStaticQuery(productQuery);
+
   return (
     <Layout>
       <SEO
@@ -13,26 +45,17 @@ function IndexPage() {
       />
 
       <section className="text-center">
-        <img
-          alt="Cat and human sitting on a couch"
-          className="block mx-auto w-1/2"
-          src={catAndHumanIllustration}
-        />
-
+        
         <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Hey there! Welcome to your first Gatsby site.
+          Integrate Gooten Api into Your Gatsby JS Page
         </h2>
-
         <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{` `}
-          <a
-            className="font-bold no-underline text-gray-900"
-            href="https://tailwindcss.com/"
-          >
-            Tailwind
-          </a>
-          , a utility-first CSS framework.
+          Below is an example of first 9 products returned from API
         </p>
+        <QueryStringToCode className="m-md p-md" title="GraphQl Query" queryString={productQueryString}/>
+        <div className="flex flex-wrap shadow-lg container w-full p-md m-md">
+          {ProductNodes.map(ProductNodeToTile)}
+        </div>
       </section>
     </Layout>
   );
