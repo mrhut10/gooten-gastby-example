e857fcebf88bc8bbc47339b9afbb7bd39636a99b
mrhut10 <mrhut10@protonmail.com>
2020-03-07 08:50:50 +1100

    change page layout example products first then query

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,19 +46,28 @@ function IndexPage() {
       />
 
       <section className="text-center">
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
+        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3 align-middle">
           Integrate Gooten Api into Your Gatsby JS Page
         </h2>
-        <p className="leading-loose">
-          Example of first 12 products returned from the Gooten API
-        </p>
-        <QueryStringToCode
-          className="m-md p-md"
-          title="GraphQl Query"
-          queryString={productQueryString}
-        />
-        <div className="flex flex-wrap shadow-lg container w-full p-md m-md">
-          {ProductNodes.map(ProductNodeToTile)}
+        <div className="w-full mx-auto">
+          <p className="leading-loose">
+            Example of first 12 products returned from the Gooten API
+          </p>
+          <div className="flex flex-wrap shadow-lg container w-full p-md m-md">
+            {ProductNodes.map(ProductNodeToTile)}
+          </div>
+          <br/>
+          <QueryStringToCode
+            className="m-md p-md font-small"
+            title="Query"
+            queryString={productQueryString}
+          >
+            <p>
+              This is the GraphQL query used for this page <br />
+              want to see full page source? click {''}
+              <a className="text-teal-500" href="https://github.com/mrhut10/gooten-gastby-example/blob/main/src/pages/index.js">here</a>
+            </p>
+          </QueryStringToCode>
         </div>
       </section>
     </Layout>
