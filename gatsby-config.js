f40432192cf009863ee36248fa99a6daf4217f4a
Luke Bennett <hello@lukebennett.com.au>
2020-03-08 21:38:38 +1100

    Update name of manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-tailwind`,
+        name: `gatsby-source-gooten`,
         short_name: `starter`,
         start_url: `/`,
         background_color: defaultTheme.colors.white,
