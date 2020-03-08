abec75785f4f859b61bcb310cf1db7fb1c9bc964
Luke Bennett <hello@lukebennett.com.au>
2020-03-08 17:26:46 +1100

    Spread in default values from Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,13 @@
 // See https://tailwindcss.com/docs/configuration for details
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   theme: {
     rotate: {
-      '-180': '-180deg',
-      '-90': '-90deg',
-      '-45': '-45deg',
-      '-5': '-5deg',
-      '0': '0',
-      '2': '2deg',
-      '5': '5deg',
-      '10': '10deg',
-      '45': '45deg',
-      '90': '90deg',
-      '135': '135deg',
-      '180': '180deg',
-      '270': '270deg',
+      "-5": "-5deg",
+      "-2": "-2deg",
+      "5": "5deg",
+      "2": "2deg",
+      ...defaultTheme.rotate
     }
   },
   variants: {},
