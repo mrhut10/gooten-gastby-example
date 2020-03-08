c9cc615cfb128967136447830d1c7f1f32c117ae
Luke Bennett <hello@lukebennett.com.au>
2020-03-08 17:31:08 +1100

    Sort classes (using Headwind VS Code extention)

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,10 +6,10 @@ import Footer from "./footer";
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col font-sans min-h-screen text-gray-900">
+    <div className="flex flex-col min-h-screen font-sans text-gray-700 bg-gray-100">
       <Header />
 
-      <main className="flex flex-col flex-1 md:justify-center max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
+      <main className="flex flex-col flex-1 w-full max-w-4xl px-4 py-8 mx-auto mt-12 md:justify-center md:p-8">
         {children}
       </main>
 
