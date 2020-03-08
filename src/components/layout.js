c932a265ffdb55e17866ce0d62e9753d50e31aac
Luke Bennett <hello@lukebennett.com.au>
2020-03-08 17:31:47 +1100

    Use antialiasing to make text look nicer

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ import Footer from "./footer";
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen font-sans text-gray-700 bg-gray-100">
+    <div className="flex flex-col min-h-screen font-sans antialiased text-gray-700 bg-gray-100">
       <Header />
 
       <main className="flex flex-col flex-1 w-full max-w-4xl px-4 py-8 mx-auto mt-12 md:justify-center md:p-8">
