2e57131945011e63f8c8c41ebf6a577b188c4f76
Luke Bennett <hello@lukebennett.com.au>
2020-03-08 17:29:30 +1100

    Fix mobile menu, style and make sticky on mobile

--- src/components/header.js
+++ src/components/header.js
@@ -16,31 +16,29 @@ function Header() {
   `);
 
   return (
-    <header className="bg-teal-700">
-      <div className="flex lex-wrap-reverse items-center justify-between max-w-4xl mx-auto p-4 md:p-8">
+    <header className="sticky top-0 z-10 bg-gray-100 shadow sm:shadow-none sm:relative">
+      <div className="flex flex-wrap items-center justify-between max-w-4xl p-4 mx-auto md:p-8">
         <Link
-          className="flex flex-wrap items-center no-underline text-white sm:flex-nowrap"
+          className="flex flex-wrap items-center font-medium text-gray-800 transition duration-150 ease-in-out transform sm:flex-nowrap hover:text-gray-600 focus:underline focus:outline-none"
           to="/"
         >
           <img
-            className="fill-current h-8 mr-2 w-8 rounded-full"
-            height="54"
+            className="h-8 mr-2 overflow-hidden rounded-full"
             src={gootenIcon}
             alt="gooten icon"
           />
           <img className="h-8" src={GatsbyIcon} />
-          <span className="w-8" />
-          <span className="font-bold text-xl tracking-tight">
+          <span className="ml-3 text-xl font-medium tracking-tight">
             {site.siteMetadata.title}
           </span>
         </Link>
 
         <button
-          className="block md:hidden border border-white flex items-center px-3 py-2 rounded text-white"
+          className="flex items-center block px-3 py-2 text-gray-500 border border-gray-400 rounded md:hidden"
           onClick={() => toggleExpansion(!isExpanded)}
         >
           <svg
-            className="fill-current h-3 w-3"
+            className="w-3 h-3 fill-current"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -65,7 +63,8 @@ function Header() {
             }
           ].map(link => (
             <Link
-              className="block md:inline-block mt-4 md:mt-0 md:ml-6 no-underline text-white"
+              className="block mt-4 font-medium text-gray-800 transition duration-150 ease-in-out transform hover:text-gray-600 focus:underline focus:outline-none md:inline-block md:mt-0 md:ml-6"
+              // className="block mt-4 no-underline md:inline-block md:mt-0 md:ml-6"
               key={link.title}
               to={link.route}
             >
